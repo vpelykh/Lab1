8b6c7d83b435ecb3288074b18087eee1decf0ef3
Vika <vika9282@ukr.net>
2022-09-22 23:10:55 +0300

    Initial commit

---
+++ Goods.h
@@ -0,0 +1,22 @@
+#ifndef LAB1_GOODS_H
+#define LAB1_GOODS_H
+#include <iostream>
+#include <string>
+using std::string;
+
+class Goods {
+    string name, registrationDate;
+    int amount, invoiceNumber;
+    double price;
+public:
+    Goods Init(string name, string registrationDate, double price, int amount, int invoiceNumber);
+    void Read();
+    void Display();
+    string toString();
+    void changePrice(double newPrice);
+    void changeAmount(int valueToAdd);
+    double calculateGoodsCost();
+};
+
+
+#endif //LAB1_GOODS_H
