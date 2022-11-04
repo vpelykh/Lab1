cadacd9b1f700dc83ef6c74bc63f0e008a6c2e68
Vika <vika9282@ukr.net>
2022-11-04 21:55:52 +0200

    Lab 5

---
+++ Lab4/Bitstring.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Админ on 03.10.2022.
+//
+
+#include "Bitstring.h"
---
+++ Lab4/Bitstring.h
@@ -0,0 +1,14 @@
+//
+// Created by Админ on 03.10.2022.
+//
+
+#ifndef OOP_LABS_BITSTRING_H
+#define OOP_LABS_BITSTRING_H
+
+
+class Bitstring {
+
+};
+
+
+#endif //OOP_LABS_BITSTRING_H
