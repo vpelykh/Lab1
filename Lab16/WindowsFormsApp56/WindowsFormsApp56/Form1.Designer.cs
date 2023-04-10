6df3c1a8060f1a543b0d7a706c8e1d6c50625108
Vika <vika9282@ukr.net>
2023-04-10 21:33:49 +0300

    Add lab 16

---
+++ Lab16/WindowsFormsApp56/WindowsFormsApp56/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace WindowsFormsApp56
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(32, 288);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 43);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(32, 347);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 37);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(32, 401);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(107, 40);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(184, 53);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(238, 22);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(184, 104);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(238, 22);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(186, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(235, 52);
+            this.listBox1.TabIndex = 9;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(186, 250);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(238, 22);
+            this.numericUpDown3.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 296);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab16/WindowsFormsApp56/WindowsFormsApp56/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp56
+{
+    public partial class Form1 : Form
+    {
+        private FractionCalculator calculator;
+        public Form1()
+        {
+            InitializeComponent();
+            calculator = new FractionCalculator();
+        }
+        public class Fraction
+        {
+            private int numerator; 
+            private int denominator;
+            public Fraction(int numerator, int denominator)
+            {
+                this.numerator = numerator;
+                this.denominator = denominator;
+            }
+            public int Numerator
+            {
+                get { return numerator; }
+                set { numerator = value; }
+            }
+            public int Denominator
+            {
+                get { return denominator; }
+                set { denominator = value; }
+            }
+          
+            public void Reduce()
+            {
+                int gcd = Gcd(numerator, denominator);
+                numerator /= gcd;
+                denominator /= gcd;
+            }
+           
+            public void Power(int n)
+            {
+                numerator = (int)Math.Pow(numerator, n);
+                denominator = (int)Math.Pow(denominator, n);
+            }
+           
+            public double ToDecimal()
+            {
+                return (double)numerator / denominator;
+            }
+ 
+            public override string ToString()
+            {
+                return string.Format("{0}/{1}", numerator, denominator);
+            }
+          
+            private static int Gcd(int a, int b)
+            {
+                while (b != 0)
+                {
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+                return a;
+            }
+          
+            public static Fraction operator +(Fraction a, Fraction b)
+            {
+                int newNumerator = a.numerator * b.denominator + b.numerator * a.denominator;
+                int newDenominator = a.denominator * b.denominator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+       
+            public static Fraction operator -(Fraction a, Fraction b)
+            {
+                int newNumerator = a.numerator * b.denominator - b.numerator * a.denominator;
+                int newDenominator = a.denominator * b.denominator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+        
+            public static Fraction operator *(Fraction a, Fraction b)
+            {
+                int newNumerator = a.numerator * b.numerator;
+                int newDenominator = a.denominator * b.denominator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+     
+            public static Fraction operator /(Fraction a, Fraction b)
+            {
+                int newNumerator = a.numerator * b.denominator;
+                int newDenominator = a.denominator * b.numerator;
+                return new Fraction(newNumerator, newDenominator);
+            }
+          
+            public static bool operator ==(Fraction a, Fraction b)
+            {
+                return a.numerator * b.denominator == b.numerator * a.denominator;
+            }
+            public static bool operator !=(Fraction a, Fraction b)
+            {
+                return !(a == b);
+            }
+            public static bool operator >=(Fraction a, Fraction b)
+            {
+                return a.numerator * b.denominator >= b.numerator * a.denominator;
+            }
+            public static bool operator <=(Fraction a, Fraction b)
+            {
+                return a.numerator * b.denominator <= b.numerator * a.denominator;
+            }
+            public static bool operator >(Fraction a, Fraction b)
+            {
+                return a.numerator * b.denominator > b.numerator * a.denominator;
+            }
+            public static bool operator <(Fraction a, Fraction b)
+            {
+                return a.numerator * b.denominator < b.numerator * a.denominator;
+            }
+        }
+        
+        public class FractionCalculator
+        {
+            private List<Fraction> fractions; 
+            public FractionCalculator()
+            {
+                fractions = new List<Fraction>();
+            }
+            public void AddFraction(int numerator, int denominator)
+            {
+                Fraction fraction = new Fraction(numerator, denominator);
+                fractions.Add(fraction);
+            }
+            public Fraction Sum()
+            {
+                Fraction result = new Fraction(0, 1);
+                foreach (Fraction fraction in fractions)
+                {
+                    result += fraction;
+                }
+                return result;
+            }
+            public Fraction Average()
+            {
+                Fraction sum = Sum();
+                int count = fractions.Count;
+                Fraction result = new Fraction(sum.Numerator, sum.Denominator * count);
+                return result;
+            }
+            public void ReduceFraction(int index)
+            {
+                Fraction fraction = fractions[index];
+                fraction.Reduce();
+            }
+            public void PowerFraction(int index, int n)
+            {
+                Fraction fraction = fractions[index];
+                fraction.Power(n);
+            }
+            public void DeleteFraction(int index)
+            {
+                fractions.RemoveAt(index);
+            }
+            public void ClearFractions()
+            {
+                fractions.Clear();
+            }
+            public int Count()
+            {
+                return fractions.Count;
+            }
+            public Fraction GetFraction(int index)
+            {
+                return fractions[index];
+            }
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (Fraction fraction in fractions)
+                {
+                    sb.AppendLine(fraction.ToString());
+                }
+                return sb.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numerator = (int)numericUpDown1.Value;
+            int denominator = (int)numericUpDown2.Value;
+            calculator.AddFraction(numerator, denominator);
+            listBox1.Items.Add(calculator.GetFraction(calculator.Count() - 1).ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Fraction sum = calculator.Sum();
+            textBox1.Text = sum.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Fraction average = calculator.Average();
+            textBox1.Text = average.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1)
+            {
+                calculator.ReduceFraction(index);
+                listBox1.Items[index] = calculator.GetFraction(index).ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            int n = (int)numericUpDown3.Value;
+            if (index != -1)
+            {
+                calculator.PowerFraction(index, n);
+                listBox1.Items[index] = calculator.GetFraction(index).ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1)
+            {
+                calculator.DeleteFraction(index);
+                listBox1.Items.RemoveAt(index);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            calculator.ClearFractions();
+            listBox1.Items.Clear();
+            textBox1.Text = string.Empty;
+        }
+    }
+}
