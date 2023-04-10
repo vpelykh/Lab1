a0bfa623a60b30f885105f1e3debc1672030111a
Vika <vika9282@ukr.net>
2023-04-10 21:40:01 +0300

    add lab 19

---
+++ Lab19/WindowsFormsApp41/WindowsFormsApp41/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp41
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string expression = textBox1.Text;
+
+            Stack<char> stack = new Stack<char>();
+            bool isCorrect = true;
+
+            foreach (char c in expression)
+            {
+                if (c == '(')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                isCorrect = false;
+            }
+
+            if (isCorrect)
+            {
+                MessageBox.Show("Дужки розставлені правильно");
+            }
+            else
+            {
+                MessageBox.Show("Дужки розставлені неправильно");
+            }
+        }
+    }
+}
