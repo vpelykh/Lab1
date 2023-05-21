07209a6071d999ca8f3cb1f2fdcdc958d5464f0d
Vika <vika9282@ukr.net>
2023-05-21 22:25:45 +0300

    Add lab21

---
+++ lab21/lab21/blank.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace lab21
+{
+    partial class blank
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmnuCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Amount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Time = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 450);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmnuCut,
+            this.cmnuCopy,
+            this.cmnuPaste,
+            this.cmnuDelete,
+            this.cmnuSelectAll});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowCheckMargin = true;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(163, 124);
+            // 
+            // cmnuCut
+            // 
+            this.cmnuCut.Name = "cmnuCut";
+            this.cmnuCut.Size = new System.Drawing.Size(162, 24);
+            this.cmnuCut.Text = "Cu&t";
+            this.cmnuCut.Click += new System.EventHandler(this.cmnuCut_Click);
+            // 
+            // cmnuCopy
+            // 
+            this.cmnuCopy.Name = "cmnuCopy";
+            this.cmnuCopy.Size = new System.Drawing.Size(162, 24);
+            this.cmnuCopy.Text = "&Copy";
+            this.cmnuCopy.Click += new System.EventHandler(this.cmnuCopy_Click);
+            // 
+            // cmnuPaste
+            // 
+            this.cmnuPaste.Name = "cmnuPaste";
+            this.cmnuPaste.Size = new System.Drawing.Size(162, 24);
+            this.cmnuPaste.Text = "&Paste";
+            this.cmnuPaste.Click += new System.EventHandler(this.cmnuPaste_Click);
+            // 
+            // cmnuDelete
+            // 
+            this.cmnuDelete.Name = "cmnuDelete";
+            this.cmnuDelete.Size = new System.Drawing.Size(162, 24);
+            this.cmnuDelete.Text = "&Delete";
+            this.cmnuDelete.Click += new System.EventHandler(this.cmnuDelete_Click);
+            // 
+            // cmnuSelectAll
+            // 
+            this.cmnuSelectAll.Name = "cmnuSelectAll";
+            this.cmnuSelectAll.Size = new System.Drawing.Size(162, 24);
+            this.cmnuSelectAll.Text = "Select &All";
+            this.cmnuSelectAll.Click += new System.EventHandler(this.cmnuSelectAll_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Amount,
+            this.Time});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Amount
+            // 
+            this.Amount.Name = "Amount";
+            this.Amount.Size = new System.Drawing.Size(138, 20);
+            this.Amount.Text = "Amount of symbols";
+            this.Amount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Time
+            // 
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(42, 20);
+            this.Time.Text = "Time";
+            // 
+            // blank
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "blank";
+            this.Text = "blank";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.blank_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cmnuCut;
+        private System.Windows.Forms.ToolStripMenuItem cmnuCopy;
+        private System.Windows.Forms.ToolStripMenuItem cmnuPaste;
+        private System.Windows.Forms.ToolStripMenuItem cmnuDelete;
+        private System.Windows.Forms.ToolStripMenuItem cmnuSelectAll;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        public System.Windows.Forms.ToolStripStatusLabel Amount;
+        public System.Windows.Forms.ToolStripStatusLabel Time;
+    }
+}
---
+++ lab21/lab21/blank.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace lab21
+{
+    public partial class blank : Form
+    {
+        public blank()
+        {
+            InitializeComponent();
+            Time.Text = Convert.ToString(System.DateTime.Now.ToLongTimeString());
+            Time.ToolTipText = Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+        public string DocName = "";
+        private string BufferText = "";
+        public bool IsSaved = false;
+        public string ask = "Do you want save changes in ";
+        public string msg = "Message";
+        public string symbols = "Аmount of symbols  ";
+        public void Cut()
+        {
+            this.BufferText = richTextBox1.SelectedText;
+            richTextBox1.SelectedText = "";
+        }
+        public void Copy()
+        {
+            this.BufferText = richTextBox1.SelectedText;
+        }
+        public void Paste()
+        {
+            richTextBox1.SelectedText = this.BufferText;
+        }
+        public void SelectAll()
+        {
+            richTextBox1.SelectAll();
+        }
+        public void Delete()
+        {
+            richTextBox1.SelectedText = "";
+            this.BufferText = "";
+        }
+
+        private void cmnuCut_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void cmnuCopy_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void cmnuPaste_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void cmnuDelete_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+
+        private void cmnuSelectAll_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+
+        public void Open(string OpenFileName)
+        {
+            if (OpenFileName == "")
+            {
+                return;
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(OpenFileName);
+                richTextBox1.Text = sr.ReadToEnd();
+                sr.Close();
+                DocName = OpenFileName;
+            }
+        }
+
+        public void Save(string SaveFileName)
+        {
+            if (SaveFileName == "")
+            {
+                return;
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(SaveFileName);
+                sw.WriteLine(richTextBox1.Text);
+                sw.Close();
+            }
+        }
+        private void blank_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (IsSaved == true)
+                if (MessageBox.Show(ask + this.DocName + "?",
+                msg, MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    this.Save(this.DocName);
+                }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Amount.Text = symbols + richTextBox1.Text.Length.ToString();
+        }
+    }
+}
